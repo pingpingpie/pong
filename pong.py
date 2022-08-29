f0b96b7f76ddfe2812446b74aa708efa9d8ae2a0
pingpingpie <112383746+pingpingpie@users.noreply.github.com>
2022-08-29 14:01:41 +0300

    Update pong.py

--- pong.py
+++ pong.py
@@ -1,11 +1,3 @@
-echo "# pong" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/pingpingpie/pong.git
-git push -u origin main
-
 import turtle
 import winsound
 
